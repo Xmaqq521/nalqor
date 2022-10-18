d19cab57c9a480648a29e09d98c6113e79800e9d
Xmaqq521 <90538838+Xmaqq521@users.noreply.github.com>
2022-10-18 00:32:46 +0000

    Add files via upload

---
+++ configure.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# V2Ray new configuration
+# Run V2ray
+# Write V2Ray configuration
+cat << EOF > /etc/v2ray/config.json
+{
+    "inbounds": [{
+        "port": 8080,
+        "protocol": "vmess",
+        "settings": {
+            "clients": [{
+                "id": "fc28df5a-913b-4d2f-ba03-43a069cf1693",
+                "alterId": 0
+            }]
+        },
+        "streamSettings": {
+            "network": "ws",
+            "wsSettings": {
+                "path": "/lawrkrc"
+            }
+        }
+    }],
+    "outbounds": [{
+        "protocol": "freedom"
+    }]
+}
+EOF
+
+#Run V2ray
+
+/usr/bin/v2ray/v2ray  run -config=/etc/v2ray/config.json
